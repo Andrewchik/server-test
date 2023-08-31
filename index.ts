a7707841bfafcf419ff7f463865c25fddfce4df0
Andrewchik <andriy.goryachiy16@gmail.com>
2023-08-31 17:36:11 +0300

    [ADD] book model

--- index.ts
+++ index.ts
@@ -1,13 +1,23 @@
 import express, { Application } from 'express';
+import cors from 'cors';
+
 import { connectDB } from './db';
+import Book from './models/book';
+import bodyParser from 'body-parser';
+
 const app: Application = express();
 const PORT: number = 5000;
+
+app.use(express.json());
+app.use(bodyParser.json());
+app.use(cors());
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
 connectDB();
+
 
 app.listen(PORT, () => {
     console.log(`Server is listening on port ${PORT}`);
---
+++ models/book.ts
@@ -0,0 +1,19 @@
+import mongoose, { Document } from 'mongoose';
+
+interface Book extends Document {
+  id: number;
+  title: string;
+  description: string;
+  author: string;
+}
+
+const bookSchema = new mongoose.Schema({
+  id: Number,
+  title: String,
+  description: String,
+  author: String,
+});
+
+const Book = mongoose.model<Book>('Book', bookSchema);
+
+export default Book;
