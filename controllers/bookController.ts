0bd7cbecf3806bbcf06208ffdd643b3108f61c02
Andrewchik <andriy.goryachiy16@gmail.com>
2023-09-01 08:04:00 +0300

    [UPD] imports

--- controllers/bookController.ts
+++ controllers/bookController.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from 'express';
 // import Book from '../models/Book';
-const Book = require('../models/Book')
+const {Book} = require('../models/Book')
 
 export const getBooks = async (req: Request, res: Response) => {  
   try {
