b50fb1383f8a24c2e863f7acec4c26f7765dfa22
Andrewchik <andriy.goryachiy16@gmail.com>
2023-09-01 23:59:36 +0300

    [ADD] update and delete controllers

--- controllers/bookController.ts
+++ controllers/bookController.ts
@@ -28,3 +28,35 @@ export const createBook = async (req: Request, res: Response) => {
     res.status(500).json({ message: 'Error creating a book', error });
   }
 };
+
+export const updateBook = async (req: Request, res: Response) => {
+  const { id } = req.params;
+
+  try {
+    const updatedBook = await Book.findByIdAndUpdate(id, req.body, { new: true });
+
+    if (!updatedBook) {
+      return res.status(404).json({ message: 'Book not found' });
+    }
+
+    return res.json(updatedBook);
+  } catch (error) {
+    return res.status(500).json({ message: 'Error updating book', error });
+  }
+};
+
+export const deleteBook = async (req: Request, res: Response) => {
+  const { id } = req.params;
+
+  try {
+    const deletedBook = await Book.findByIdAndRemove(id);
+
+    if (!deletedBook) {
+      return res.status(404).json({ message: 'Book not found' });
+    }
+
+    return res.status(204).send();
+  } catch (error) {
+    return res.status(500).json({ message: 'Error deleting book', error });
+  }
+};
--- routes/bookRoutes.ts
+++ routes/bookRoutes.ts
@@ -1,10 +1,14 @@
 import { Router } from 'express';
-import { getBooks, createBook } from '../controllers/bookController';
+import { getBooks, createBook, updateBook, deleteBook } from '../controllers/bookController';
 
 const router = Router();
 
 router.get('/', getBooks);
 
 router.post('/create', createBook);
+
+router.put('/:id', updateBook);
+
+router.delete('/:id', deleteBook);
 
 export default router;
